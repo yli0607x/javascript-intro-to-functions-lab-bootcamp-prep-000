a9a0e7811bef349f7d7ee9858fcb6333e6d77fda
Yu Li <yli0607x@gmail.com>
2018-09-20 01:04:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
-  return "I can't hear you!"
-}
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
 
-if (string.toUpperCase() === string) {
- return "YES INDEED!"
-}
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
 
-if (string === "I love you, Grandama."){
-  return "I love you, too."
-}
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
