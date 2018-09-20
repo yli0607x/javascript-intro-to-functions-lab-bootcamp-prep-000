2188835748e10a20ed4887c180af741661df5d3d
Yu Li <yli0607x@gmail.com>
2018-09-20 00:58:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string.toLowerCase() === lowercase
+  if (string.toLowerCase() === string)
   return "I can't hear you!"
 }
